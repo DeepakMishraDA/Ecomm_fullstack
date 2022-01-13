0a6fd3e350a32cabfe859a239c52b24055d8b23f
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-01-13 17:22:04 +0100

    commit

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,15 +1,28 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import Users from '../models/users'
 import { createUser } from '../services/user'
+import { BadRequestError } from '../helpers/apiError'
 
-export const createUserDoc = async (req: Request, res: Response) => {
-  const { name, email, phone, address } = req.body
-  const user = new Users({
-    name,
-    email,
-    phone,
-    address,
-  })
-  await createUser(user)
-  res.json(user)
+export const createUserDoc = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, phone, address } = req.body
+    const user = new Users({
+      name,
+      email,
+      phone,
+      address,
+    })
+    await createUser(user)
+    res.json(user)
+  } catch (error) {
+    if (error instanceof Error && error.name == 'ValidationError') {
+      next(new BadRequestError('Invalid Request', error))
+    } else {
+      next(error)
+    }
+  }
 }
