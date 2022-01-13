9067b7b7875d635d1112810c1ce3463da65e6fe6
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-01-13 17:27:59 +0100

    commit

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,5 +1,9 @@
-import { UserDocument } from '../models/users'
+import Users, { UserDocument } from '../models/users'
 
 export const createUser = async (user: UserDocument): Promise<UserDocument> => {
   return user.save()
+}
+
+export const findAll = async (): Promise<UserDocument[]> => {
+  return Users.find()
 }
