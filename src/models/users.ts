53d6e6d01e09449d6086749beaf23055a5a7b39a
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-01-15 12:43:28 +0100

    commit

--- src/models/users.ts
+++ src/models/users.ts
@@ -3,18 +3,18 @@ import mongoose, { Document } from 'mongoose'
 export type UserDocument = Document & {
   name: string
   email: string
-  //phone: number
-  //date: Date
-  //address: string[]
+  phone: number
+  date: Date
+  address: string[]
 }
 const { Schema } = mongoose
 
 const users = new Schema({
   name: String,
   email: { type: String, unique: true, required: true },
-  //phone: Number,
-  //date: Date,
-  //address: Array,
+  phone: Number,
+  date: Date,
+  address: Array,
 })
 
 export default mongoose.model<UserDocument>('Users', users)
