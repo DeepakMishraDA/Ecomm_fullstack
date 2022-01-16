9168a96bb1d85dd029a3b8c5a3d3651e98502be4
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-01-16 21:22:23 +0100

    products model added

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,7 +1,11 @@
-import { ProdDocument } from '../models/products'
+import Products, { ProdDocument } from '../models/products'
 
 export const createProduct = async (
   prod: ProdDocument
 ): Promise<ProdDocument> => {
   return prod.save()
+}
+
+export const findAll = async (): Promise<ProdDocument[]> => {
+  return Products.find()
 }
