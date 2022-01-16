750f12f5300e4bd53a8e0f59d6b61fa0cf12ce4b
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-01-16 21:14:31 +0100

    products model added

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,3 +1,29 @@
-export const createProdDoc = () => {
-  return
+import { Request, Response, NextFunction } from 'express'
+import Products from '../models/products'
+import { createProduct } from '../services/products'
+import { BadRequestError } from '../helpers/apiError'
+
+export const createProdDoc = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { title, prodid, category, specifications, price } = req.body
+    const products = new Products({
+      title,
+      prodid,
+      category,
+      specifications,
+      price,
+    })
+    await createProduct(products)
+    res.json(products)
+  } catch (error) {
+    if (error instanceof Error && error.name == 'ValidationError') {
+      next(new BadRequestError('Invalid Request', error))
+    } else {
+      next(error)
+    }
+  }
 }
