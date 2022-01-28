68e32a31103d5408321692be201d558c9b6003df
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-01-29 00:50:28 +0100

    commit

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -10,9 +10,11 @@ const passportStrgy = new GoogleTokenStrategy(
     clientID: process.env.GOOGLE_CLIENT_ID,
     //getGoogleCerts: optionalCustomGetGoogleCerts,
   },
-  function (parsedToken, googleId, done) {
-    User.findOrCreate({ googleId: googleId }, function (err, user) {
-      return done(err, user)
-    })
+  function (parsedToken: any, googleId: any, done: any) {
+    console.log('parsedToken', parsedToken)
+    done(null, {})
   }
 )
+//  User.findOrCreate({ googleId: googleId }, function (err, user) {
+//    return done(err, user)
+//  })
