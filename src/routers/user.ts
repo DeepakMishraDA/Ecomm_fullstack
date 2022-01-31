f557b2ec091609c1588da9b9fed1e224e0464ba6
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-01-31 02:59:33 +0100

    commit

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { createUserDoc } from '../controllers/users'
+import { createUserDoc, userFindall } from '../controllers/users'
 
 const users = express.Router()
 
+users.get('/allusers', userFindall)
 users.post('/', createUserDoc)
 
 export default users
