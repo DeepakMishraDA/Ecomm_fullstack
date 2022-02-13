353f938288a4bad4a818844e2bea3f3c0be70afd
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-02-13 08:39:52 +0100

    commit

--- src/routers/products.ts
+++ src/routers/products.ts
@@ -1,9 +1,9 @@
 import express from 'express'
-import { createProdDoc, findAllprod } from '../controllers/products'
+import { createProdDoc, findAllprod, updateProd } from '../controllers/products'
 
 const products = express.Router()
 
 products.get('/allproducts', findAllprod)
 products.post('/', createProdDoc)
-
+products.post('/:prodId', updateProd)
 export default products
