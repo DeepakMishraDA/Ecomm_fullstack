ce9ac1e8bb70de4523a01aae9c0f58b25548daab
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-02-15 05:07:24 +0100

    commit

--- src/services/order.ts
+++ src/services/order.ts
@@ -1,6 +1,12 @@
 import Order, { OrderDocument } from '../models/order'
 import { NotFoundError } from '../helpers/apiError'
 
-export const createOrd = async (order: OrderDocument) => {
+export const createOrd = async (
+  order: OrderDocument
+): Promise<OrderDocument> => {
   return order.save()
+}
+
+export const findallOrd = async (): Promise<OrderDocument[]> => {
+  return Order.find()
 }
